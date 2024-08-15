091d2496641c77a32f1a07b73daac8dbd4f84175
Pedro Mantovani <pedromantovani26@gmail.com>
2024-08-15 14:29:36 -0300

    feat : add side projects

---
+++ src/components/projects/index.tsx
@@ -0,0 +1,17 @@
+const Projects = () => {
+    return ( 
+        <section className="max-w-3xl px-0 md:px-4 mt-20">
+            <div className="bg-zinc-100 p-6 rounded-2xl">
+                <h3 className="font-medium text-slate-800">Side Projects</h3>
+                <p className="leading-7 text-slate-500">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut voluptatibus dolores cumque veritatis dolor, consectetur dolorum alias recusandae temporibus, ab quae porro inventore sint, accusantium quibusdam quia magni id et.</p>
+                <div className="mt-4 grid grid-cols-1 gap-3 sm:grid-cols-2">
+                    <article className="flex h-full flex-col gap-4 rounded-2xl border border-slate-200 bg-white p-3 shadow-md shadow-slate-200">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum repellat aliquid excepturi. Facilis dignissimos non modi, animi officiis corporis velit error beatae aliquid repellat at cupiditate pariatur in vitae accusantium!
+                    </article>
+                </div>
+            </div>
+        </section>
+     );
+}
+ 
+export default Projects;
