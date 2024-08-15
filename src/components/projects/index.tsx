232d78dd5282883e2a9a5eced95cf7eca6e3d2c7
Pedro Mantovani <pedromantovani26@gmail.com>
2024-08-15 17:04:13 -0300

    feat : add projects

--- src/components/projects/index.tsx
+++ src/components/projects/index.tsx
@@ -1,12 +1,64 @@
+import { TbExternalLink } from "react-icons/tb";
+import Image from "next/image";
+import WebstreamsImage from '../../../public/json.png'
+import ShopImage from '../../../public/shop.png'
+import BarberImage from '../../../public/barber.png'
+import MailImage from '../../../public/mail.png'
+
 const Projects = () => {
     return ( 
         <section className="max-w-3xl px-0 md:px-4 mt-20">
             <div className="bg-zinc-100 p-6 rounded-2xl">
                 <h3 className="font-medium text-slate-800">Side Projects</h3>
-                <p className="leading-7 text-slate-500">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut voluptatibus dolores cumque veritatis dolor, consectetur dolorum alias recusandae temporibus, ab quae porro inventore sint, accusantium quibusdam quia magni id et.</p>
+                <p className="leading-7 text-slate-500">Discover side projects I've been working on</p>
                 <div className="mt-4 grid grid-cols-1 gap-3 sm:grid-cols-2">
                     <article className="flex h-full flex-col gap-4 rounded-2xl border border-slate-200 bg-white p-3 shadow-md shadow-slate-200">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum repellat aliquid excepturi. Facilis dignissimos non modi, animi officiis corporis velit error beatae aliquid repellat at cupiditate pariatur in vitae accusantium!
+                        <Image src={ShopImage} alt="" quality={100} className="rounded-2xl h-[100px] object-cover" />
+                        <h4 className="text-base font-medium text-slate-800">Gaming e-commerce</h4>
+                        <p  className="mt-1 text-sm text-slate-500">Full e-commerce integrated with stripe</p>
+                        <div className="flex w-full items-center justify-between">
+                            <div className="rounded-full px-2 py-1 bg-gray-100"><span className="block text-sm font-semibold text-gray-700">Next js</span></div>
+                            <a href="#" className="inline-flex items-center gap-2 rounded-sm outline-none focus-visible:ring-2 focus-visible:ring-slate-500 focus-visible:ring-offset-2" target="_blank">
+                                <span className="text-xs font-normal text-slate-500">View on GitHub</span> 
+                                <TbExternalLink color="gray"size={14}/>
+                            </a>
+                        </div>
+                    </article>
+                    <article className="flex h-full flex-col gap-4 rounded-2xl border border-slate-200 bg-white p-3 shadow-md shadow-slate-200">
+                        <Image src={BarberImage} alt="" quality={100} className="rounded-2xl h-[100px] object-cover" />
+                        <h4 className="text-base font-medium text-slate-800">Barbershop scheduling system</h4>
+                        <p  className="mt-1 text-sm text-slate-500">Some features such as Appointment Scheduling Service Selection</p>
+                        <div className="flex w-full items-center justify-between">
+                            <div className="rounded-full px-2 py-1 bg-gray-100"><span className="block text-sm font-semibold text-gray-700">Next js</span></div>
+                            <a href="#" className="inline-flex items-center gap-2 rounded-sm outline-none focus-visible:ring-2 focus-visible:ring-slate-500 focus-visible:ring-offset-2" target="_blank">
+                                <span className="text-xs font-normal text-slate-500">View on GitHub</span> 
+                                <TbExternalLink color="gray" size={14}/>
+                            </a>
+                        </div>
+                    </article>
+                    <article className="flex h-fit flex-col gap-4 rounded-2xl border border-slate-200 bg-white p-3 shadow-md shadow-slate-200">
+                        <Image src={WebstreamsImage} quality={100} alt="" className="rounded-2xl h-[100px] object-cover" />
+                        <h4 className="text-base font-medium text-slate-800">Webstreams Api</h4>
+                        <p  className="mt-1 text-sm text-slate-500">Consuming 100gb on frontend using Node Webstreams</p>
+                        <div className="flex w-full items-center justify-between">
+                            <div className="rounded-full px-2 py-1 bg-green-100"><span className="block text-sm font-semibold text-green-700">Node js</span></div>
+                            <a href="#" className="inline-flex items-center gap-2 rounded-sm outline-none focus-visible:ring-2 focus-visible:ring-slate-500 focus-visible:ring-offset-2" target="_blank">
+                                <span className="text-xs font-normal text-slate-500">View on GitHub</span> 
+                                <TbExternalLink color="gray" size={14}/>
+                            </a>
+                        </div>
+                    </article>
+                    <article className="flex min-h-fit flex-col gap-4 rounded-2xl border border-slate-200 bg-white p-3 shadow-md shadow-slate-200">
+                        <Image src={MailImage} quality={100} alt="" className="rounded-2xl h-[100px] object-cover" />
+                        <h4 className="text-base font-medium text-slate-800">Trigger mail Api</h4>
+                        <p  className="mt-1 text-sm text-slate-500">Email trigger microservice using SOLID</p>
+                        <div className="flex w-full items-center justify-between">
+                            <div className="rounded-full px-2 py-1 bg-blue-100"><span className="block text-sm font-semibold text-blue-700">Typescript</span></div>
+                            <a href="#" className="inline-flex items-center gap-2 rounded-sm outline-none focus-visible:ring-2 focus-visible:ring-slate-500 focus-visible:ring-offset-2" target="_blank">
+                                <span className="text-xs font-normal text-slate-500">View on GitHub</span> 
+                                <TbExternalLink color="gray" size={14}/>
+                            </a>
+                        </div>
                     </article>
                 </div>
             </div>
