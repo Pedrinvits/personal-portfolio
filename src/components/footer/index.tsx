6b3e1d0ed10c4a81bbee7ec5e6028f090be55024
Pedro Mantovani <pedromantovani26@gmail.com>
2024-08-15 14:29:05 -0300

    feat : add footer component

---
+++ src/components/footer/index.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return ( 
+        <footer className="mt-4 text-center text-sm text-gray-400">
+                {`© ${new Date().getFullYear()}`}
+        </footer>
+     );
+}
+ 
+export default Footer;
