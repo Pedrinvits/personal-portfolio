117133498c932dbf21e2849bae088126b8bf410d
pedro <pedromantovani26@gmail.com>
2024-08-18 09:22:39 -0300

    feat : change message footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
         <footer className="mt-4 text-center text-sm text-gray-400 overflow-hidden z-[1] relative">
                 <div className="flex flex-col gap-4">
                    <a href="mailto:pedromantovani26@gmail.com" className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 h-10 px-4 py-2">
-                    let's talk
+                    Let us chat
                    </a>
                     {`© ${new Date().getFullYear()}`}
                 </div>
