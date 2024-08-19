a77b057e21bdd121a80c5ed272d6eedafedadf87
Pedro Mantovani <pedromantovani26@gmail.com>
2024-08-19 14:26:41 -0300

    feat : add my new article in posts

--- src/components/posts/index.tsx
+++ src/components/posts/index.tsx
@@ -14,6 +14,15 @@ const Posts = () => {
                         <p className="text-sm text-slate-500">This short article aims to highlight the importance of SEO and how it is left aside by developers</p>
                     </article>
                 </Link>
+                <Link href="https://medium.com/@pedromantovani26/the-security-of-your-code-is-in-your-hands-7a5c629b2a64" className="group inline-flex items-start gap-3 rounded-lg p-4 outline-none focus-visible:ring-2 focus-visible:ring-slate-500 focus-visible:ring-offset-2 hover:shadow-md hover:shadow-slate-200">
+                    <span className="mt-[2px] flex-shrink-0 text-sm font-medium text-slate-400">
+                    08-19-2024 
+                    </span>
+                    <article>
+                        <h4 className="font-medium group-hover:underline group-hover:underline-offset-2">The Security of Your Code is in Your Hands</h4>
+                        <p className="text-sm text-slate-500">This article emphasizes the critical importance of code security in software development, highlighting that security is not just the responsibility of the security team but a shared duty for all developers</p>
+                    </article>
+                </Link>
             </article>
         </section>
      );
