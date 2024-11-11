c979c665e7c2b7713b413ac46b2b7023c557bb78
Pedro Mantovani <pedro.mantovani@contabilivre.com.br>
2024-11-11 11:15:42 -0300

    fix : component height in mobile

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -8,7 +8,7 @@ import { Suspense } from "react";
 
 export default function chat () {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-6 sm:p-24">
+        <main className="flex min-h-fit flex-col items-center justify-between p-6 sm:p-24">
             <div className="flex w-full justify-between">
                 <Button className="self-start bg-card hover:bg-card shadow-md">
                     <Link href={'/'}><span className="text-xs font-normal text-foreground flex items-center justify-center gap-2 hover:text-slate-500"><HomeIcon size={15} />Home</span>
