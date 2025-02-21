7dab75b800fc4ad77a1b9e9d927e51556f82a69d
Pedro Mantovani <pedromantovani26@gmail.com>
2025-02-21 05:55:54 -0300

    feat : change go back home icons

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -5,13 +5,14 @@ import { Button } from "@/components/ui/button";
 import { HomeIcon } from "lucide-react";
 import Link from "next/link";
 import { Suspense } from "react";
+import { ChevronLeft } from "lucide-react";
 
 export default function chat () {
     return (
         <main className="flex min-h-fit flex-col items-center justify-between p-6 sm:p-24">
             <div className="flex w-full justify-between">
                 <Button className="self-start bg-card hover:bg-card shadow-md">
-                    <Link href={'/'}><span className="text-xs font-normal text-foreground flex items-center justify-center gap-2 hover:text-slate-500"><HomeIcon size={15} />Home</span>
+                    <Link href={'/'}><span className="text-xs font-normal text-foreground flex items-center justify-center gap-2 hover:text-slate-500"><ChevronLeft size={15} /></span>
                     </Link>
                 </Button>
                 <ModeToggle />
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -2,7 +2,7 @@ import { ModeToggle } from "@/components/mode-toggle";
 import Projects from "@/components/projects";
 import { ProjectList } from "./projects";
 import Link from "next/link";
-import { HomeIcon, MoveLeft } from "lucide-react";
+import { HomeIcon, ChevronLeft } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Suspense } from 'react'
 
@@ -11,7 +11,7 @@ const ProjectsImade = () => {
         <main className="flex min-h-screen flex-col items-center justify-between p-6 sm:p-24">
             <div className="flex w-full justify-between">
                 <Button className="self-start bg-card hover:bg-card shadow-md">
-                    <Link href={'/'}><span className="text-xs font-normal text-foreground flex items-center justify-center gap-2 hover:text-slate-500"><HomeIcon size={15} />Home</span>
+                    <Link href={'/'}><span className="text-xs font-normal text-foreground flex items-center justify-center gap-2 hover:text-slate-500"><ChevronLeft size={15} /></span>
                     </Link>
                 </Button>
                 <ModeToggle />
